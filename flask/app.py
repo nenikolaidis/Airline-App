4fab089ffb1ddd15ecfcee0efa0d383da07e0e8d
Nearchos Nikolaidis <a.nikolaidis02@gmail.com>
2023-06-05 22:58:17 +0300

    init

--- flask/app.py
+++ flask/app.py
@@ -1,2 +1,16 @@
 from flask import Flask, jsonify, request
 from pymongo import MongoClient
+
+client = MongoClient("172.19.0.1",27017)
+db = client["database"]
+users_collection = db["users_collection"]
+flights_collection = db["flights_collection"]
+
+app = Flask(__name__)
+
+@app.route("/")
+def base():
+    pass
+
+if __name__ == "__main__":
+    app.run(host = "0.0.0.0", debug = True, port = 5000)
